a82b9265a85ad0807307eb8ba95ad6126a59e5d7
Serhan <72806447+mehmetserhan@users.noreply.github.com>
2024-02-24 13:12:27 +0300

    Delete Week09 directory

--- Week09/Motor.java
+++
@@ -1,8 +0,0 @@
-public class Motor{
-    public void run(){
-        System.out.println("Running the motor.");
-    }
-    public void stop(){
-        System.out.println("Stopping the motor.");
-    }
-}
