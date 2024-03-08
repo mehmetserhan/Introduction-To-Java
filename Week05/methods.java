8fcb6ab866bf74e2e216913349eaead02a18a57b
mehmetserhan <serhantolun7@gmail.com>
2024-03-08 19:31:47 +0300

    Update by mehmetserhan

--- Week05/methods.java
+++ Week05/methods.java
@@ -1,10 +1,10 @@
-class Method2{
+class Method1{
     static int multi(int a, int b){
         return a * b;
     }
 }
 
-class Method3{
+class Method2{
     int fact(int a){
         if(a == 1)
             return 1;
@@ -13,16 +13,18 @@ class Method3{
     }
 }
 
-public class methods{
+public class Methods{
     static int add(int a, int b){
         return a + b;
     }
 
     public static void main(String args[]){
-        System.out.println(add(3, 4));                   // add method is already declared in main, there is no need to write Method.add(a, b)
-        System.out.println(methods.add(3, 4));           // same output as above
-        System.out.println(Method2.multi(4, 5));
-        Method3 x = new Method3();                           // Method3 is not static, create an object first to use it
+        System.out.println(add(3, 4));                   // add method is already declared in main, there is no need to write Methods.add(a, b)
+        System.out.println(Methods.add(3, 4));           // same output as above
+        
+        System.out.println(Method1.multi(4, 5));
+        
+        Method2 x = new Method2();                           // Method2 is not static. To use it, create an object first 
         System.out.println(x.fact(5));
     }
 }
